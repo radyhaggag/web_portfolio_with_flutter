1cda5f6d0800ed52856296fafad690a83f467b04
radyhaggag <radyhaggag948@gmail.com>
2023-02-27 21:31:33 +0200

    finish from text styles

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -2,9 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:portfolio/core/utils/app_colors.dart';
 
 abstract class AppStyles {
-  static TextStyle title = TextStyle(
+  static TextStyle s28 = TextStyle(
     color: AppColors.primaryColor,
     fontSize: 28,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle s24 = TextStyle(
+    color: AppColors.white,
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle s18 = TextStyle(
+    color: AppColors.white,
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle s17 = TextStyle(
+    color: AppColors.white,
+    fontSize: 17,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle s16 = TextStyle(
+    color: AppColors.white,
+    fontSize: 16,
     fontWeight: FontWeight.w600,
   );
 }
