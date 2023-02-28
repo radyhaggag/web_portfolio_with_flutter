1818e75af58a042402aa6663f24151105554fb53
radyhaggag <radyhaggag948@gmail.com>
2023-02-28 21:32:16 +0200

    change whit splash screen

--- lib/presentation/widgets/app_bar/developer_name_btn.dart
+++ lib/presentation/widgets/app_bar/developer_name_btn.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/core/utils/app_colors.dart';
+import 'package:portfolio/core/utils/app_extensions.dart';
 import 'package:universal_html/html.dart' as html;
 
 import '../../../core/utils/app_strings.dart';
@@ -17,7 +19,9 @@ class DeveloperNameBtn extends StatelessWidget {
         padding: const EdgeInsets.symmetric(vertical: 13),
         child: Text(
           AppStrings.developerName,
-          style: AppStyles.s28,
+          style: context.width > 768
+              ? AppStyles.s28
+              : AppStyles.s24.copyWith(color: AppColors.primaryColor),
         ),
       ),
     );
