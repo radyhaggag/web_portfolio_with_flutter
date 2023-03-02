c507f6662e11c232f46f6346654dcccaf6772c01
radyhaggag <radyhaggag948@gmail.com>
2023-03-02 03:22:26 +0200

    fix imports

--- lib/presentation/widgets/body/about_me/basic_service_item.dart
+++ lib/presentation/widgets/body/about_me/basic_service_item.dart
@@ -2,10 +2,8 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-import '../../../../core/utils/app_extensions.dart';
 import '../../../../data/models/custom_service.dart';
 import '../../../../core/utils/app_colors.dart';
-import '../../../../core/utils/app_enums.dart';
 import '../../../../core/utils/app_styles.dart';
 
 class BasicServiceItem extends StatefulWidget {
