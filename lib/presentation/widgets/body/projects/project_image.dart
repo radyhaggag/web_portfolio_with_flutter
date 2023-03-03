a342c21627f5d440f45b1999799549410e5548ff
radyhaggag <radyhaggag948@gmail.com>
2023-03-03 18:33:08 +0200

    add error icon on project image

--- lib/presentation/widgets/body/projects/project_image.dart
+++ lib/presentation/widgets/body/projects/project_image.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/app_colors.dart';
+
 class ProjectImage extends StatelessWidget {
   const ProjectImage({super.key, required this.imageUrl});
   final String imageUrl;
@@ -14,6 +16,13 @@ class ProjectImage extends StatelessWidget {
         imageUrl: imageUrl,
         width: double.infinity,
         fit: BoxFit.cover,
+        errorWidget: (context, url, error) {
+          return Icon(
+            Icons.error,
+            color: AppColors.darkColor,
+            size: 50,
+          );
+        },
       ),
     );
   }
